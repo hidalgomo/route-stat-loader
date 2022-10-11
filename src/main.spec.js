2ce083968d415a04ecccdfc03f33dd8f036eac27
Moises H <moises.h@outlook.com>
2022-10-11 14:54:28 -0400

    Test files change

--- src/main.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('store', function() {
-    let a;
-    it("and so is a spec", function() {
-      a = true;
-      expect(a).toBe(true);
-    });
-});
---
+++ src/modal-bg/modal-bg.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('store', function() {
+    let a;
+    it("and so is a spec", function() {
+      a = true;
+      expect(a).toBe(true);
+    });
+});
