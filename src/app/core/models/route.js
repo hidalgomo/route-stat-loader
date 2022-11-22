ab9801e878c678bb678f51e60f469d0316445afa
Moises H <moises.h@outlook.com>
2022-11-21 20:51:23 -0500

    added class and new build

--- src/app/core/models/route.js
+++ src/app/core/models/route.js
@@ -35,8 +35,8 @@ export class Route {
 
         routeContainer.innerHTML = `
             <div class="route-label">${ this[labelPropName] }</div>  
-            <a href="http://10.155.228.80:4200/ruto-pub/percent-complete/${ this.mongo_id }?startTime=${ this.dateTimeStamp.start }&endTime=${ this.dateTimeStamp.end }&equipment=${ this.equipments.join(',') }" title="Route Map">Route Map</a>
-            <a href="http://10.155.228.80:4200/ruto-pub/percent-complete/${ this.mongo_id }?startTime=${ this.dateTimeStamp.start }&endTime=${ this.dateTimeStamp.end }&equipment=${ this.equipment_id }" title="Truck Map">Truck Map</a>
+            <a class="link route-map" href="http://10.155.228.80:4200/ruto-pub/percent-complete/${ this.mongo_id }?startTime=${ this.dateTimeStamp.start }&endTime=${ this.dateTimeStamp.end }&equipment=${ this.equipments.join(',') }" title="Route Map">Route Map</a>
+            <a class="link truck-map" href="http://10.155.228.80:4200/ruto-pub/percent-complete/${ this.mongo_id }?startTime=${ this.dateTimeStamp.start }&endTime=${ this.dateTimeStamp.end }&equipment=${ this.equipment_id }" title="Truck Map">Truck Map</a>
             <div class="outer-progress-bar">
                 <div class="inner-progress-bar ${ this.#evalClass(this[percentPropName]) }"></div>
                 <div class="progress-percent">${ (this[percentPropName] * 100).toFixed(1) }%</div>
